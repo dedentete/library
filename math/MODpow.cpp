e9177b66eff55b4bf19fa0f02dcd4a44061eaee3
dedentete <51981069+dedentete@users.noreply.github.com>
2019-11-26 22:06:04 +0900

    no message

---
+++ math/MODpow.cpp
@@ -0,0 +1,10 @@
+template <typename T>
+T MODpow(T x, T n, T MOD = 1000000007) {
+    T res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * x % MOD;
+        x = x * x % MOD;
+        n >>= 1;
+    }
+    return res;
+}
