7f92384fca5e658f721a33233b18445fd008139f
dedentete <51981069+dedentete@users.noreply.github.com>
2020-01-14 04:32:55 +0900

    no message

---
+++ tree/LowestCommonAncestor.cpp
@@ -0,0 +1,48 @@
+struct LCA{
+    int n,h;
+    vector<vector<int>> G,par;
+    vector<int> depth;
+
+    LCA(int n) : n(n), G(n), depth(n){
+        h = 1;
+        while((1 << h) <= n) h++;
+        par.assign(h, vector<int>(n, -1));
+    }
+
+    void add_edge(int u, int v){
+        G[u].emplace_back(v);
+        G[v].emplace_back(u);
+    }
+
+    void dfs(int v, int p, int d){
+        par[0][v] = p;
+        dep[v] = d;
+        for(int u : G[v]){
+            if(u != p) dfs(u, v, d + 1);
+        }
+    }
+
+    void build(int r = 0){
+        dfs(r, -1, 0);
+        for(int k = 0; k + 1 < h; k++){
+            for(int v = 0; v < n; v++){
+                if(par[k][v] != -1) par[k + 1][v] = par[k][par[k][v]];
+            }
+        }
+    }
+
+    int query(int u, int v){
+        if(depth[u] > depth[v]) swap(u, v);
+        for(int k = 0; k < h; k++){
+            if((depth[v] - depth[u]) >> k & 1) v = par[k][v];
+        }
+        if(u == v) return u;
+        for(int k = h - 1; k >= 0; k--){
+            if(par[k][u] != par[k][v]){
+                u = par[k][u];
+                v = par[k][v];
+            }
+        }
+        return par[0][u];
+    }
+}
