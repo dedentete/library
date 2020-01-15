190ee42398355bb7d9d0d752a734b35025563458
dedentete <51981069+dedentete@users.noreply.github.com>
2020-01-15 17:51:29 +0900

    no message

---
+++ other/gridBFS.cpp
@@ -0,0 +1,20 @@
+vector<vector<int>> gridBFS(vector<string> & s, int sy, int sx, char wall = '#'){
+    const int dy[] = {0, 1, 0, -1}, dx[] = {1, 0, -1, 0};
+    int h = s.size(), w = s[0].size();
+    vector<vector<int>> dist(h, vector<int>(w, -1));
+    queue<pair<int, int>> que;
+    que.emplace(sy, sx);
+    dist[sy][sx] = 0;
+    while(!que.empty()){
+        pair<int, int> p = que.front();
+        que.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = p.first + dy[i], nx = p.second + dx[i];
+            if(0 <= ny && ny < h && 0 <= nx && nx < w && s[ny][nx] != wall && dist[ny][nx] == -1){
+                que.emplace(ny, nx);
+                dist[ny][nx] = dist[p.first][p.second] + 1;
+            }
+        }
+    }
+    return dist;
+}
