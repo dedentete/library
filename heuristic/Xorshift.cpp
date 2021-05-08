756ed61ca9f1b68e1fc68eae1e93ce2533603539
dedentete <51981069+dedentete@users.noreply.github.com>
2021-05-08 17:15:37 +0900

    update

---
+++ heuristic/Xorshift.cpp
@@ -0,0 +1,21 @@
+struct XorShift {
+    unsigned int x, y, z, w, t;
+
+    XorShift(int seed) {
+        mt19937 rnd(seed);
+        x = rnd();
+        y = rnd();
+        z = rnd();
+        w = rnd();
+        t = 1;
+    }
+
+    int rand() {
+        t = x ^ (x << 11);
+        x = y;
+        y = z;
+        z = w;
+        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+        return w & 0x7fffffff;
+    }
+} rnd(rand());
---
+++ math/Divisor.cpp
@@ -0,0 +1,11 @@
+vector<long long> Divisor(long long n) {
+    vector<long long> res;
+    for (int i = 1; (long long)i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i != n / i) res.push_back(n / i);
+        }
+    }
+    sort(begin(res), end(res));
+    return res;
+}
